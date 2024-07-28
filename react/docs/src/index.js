aa84fd9941f12f6630f4175b0dde3f9fe3e7bfda
HajiMohamedRufai <mohamedrufai59@gmail.com>
2024-07-28 08:38:44 +0300

    -m

--- react/docs/src/index.js
+++ react/docs/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 // ReactDOM.render(<WHAT TO SHOW>, <WHERE TO SHOW>)
-ReactDOM.render(JSX, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
-<div></div>
+
+
---
+++ react/test.js
@@ -0,0 +1,8 @@
+// Creating a new datetime
+// Create a new Date for January 1, 2000, at 13:37:42
+// Note: Months are 0-indexed (0 = January)
+let myDate = new Date(2000, 0, 1, 13, 37, 42); 
+console.log(myDate); // Outputs: Sat Jan 01 2000 13:37:42 GMT...
+
+myDate = new Date(2024, 6, 22, 15, 45, 30);
+console.log(myDate);
