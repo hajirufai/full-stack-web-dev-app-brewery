0f2fcda8726126838252fb3db4cd3b836c236596
HajiMohamedRufai <mohamedrufai59@gmail.com>
2024-08-25 06:46:19 +0300

    -m

--- web3-blockchain/simple-backend/my-react-app/src/index.js
+++ web3-blockchain/simple-backend/my-react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./components/App.jsx";
 
 // ReactDOM.render(<WHAT TO SHOW>, <WHERE TO SHOW>)
 ReactDOM.render(<App />, document.getElementById("root"));
